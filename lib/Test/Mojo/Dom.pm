e49abd3b4158e7cb2dc4c8de3f81e6e73191f3e0
sugama <sugama@sv02.cutout.biz>
2012-04-28 20:02:41 +0900

    dom is always a collection

--- lib/Test/Mojo/Dom.pm
+++ lib/Test/Mojo/Dom.pm
@@ -3,7 +3,7 @@ use Mojo::Base 'Test::Mojo';
   
   sub test_dom {
     my ($self, $cb) = @_;
-    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    #local $Test::Builder::Level = $Test::Builder::Level + 1;
     $cb->(Test::Mojo::Dom::_Test->new($self->tx->res->dom));
   }
 
@@ -16,79 +16,82 @@ use Mojo::DOM;
   sub new {
     my ($class, $dom) = @_;
     my $self = $class->SUPER::new;
-    $self->dom($dom || Mojo::DOM->new);
+    if (! $dom->isa('Mojo::Collection')) {
+      $dom = Mojo::Collection->new($dom || Mojo::DOM->new);;
+    }
+    $self->dom($dom);
     return $self;
   }
   
   sub at {
     my ($self, $selector) = @_;
-    return __PACKAGE__->new($self->{dom}->at($selector));
+    return __PACKAGE__->new($self->dom->[0]->find($selector));
   }
   
   sub children {
     my ($self, $selector) = @_;
-    return __PACKAGE__->new($self->{dom}->children($selector));
+    return __PACKAGE__->new($self->dom->[0]->children($selector));
   }
   
   sub each {
     my ($self, $cb) = @_;
-    return __PACKAGE__->new($self->{dom}->each(sub {
+    return __PACKAGE__->new($self->dom->each(sub {
       $cb->(__PACKAGE__->new(shift));
     }));
   }
   
   sub get {
     my ($self, $index) = @_;
-    return __PACKAGE__->new($self->{dom}->[$index]);
+    return __PACKAGE__->new($self->dom->[$index]);
   }
   
   sub find {
     my ($self, $selector) = @_;
-    return __PACKAGE__->new($self->{dom}->find($selector));
+    return __PACKAGE__->new($self->dom->[0]->find($selector));
   }
   
   sub parent {
     my ($self) = @_;
-    return __PACKAGE__->new($self->{dom}->parent);
+    return __PACKAGE__->new($self->dom->[0]->parent);
   }
   
   sub root {
     my ($self) = @_;
-    return __PACKAGE__->new($self->{dom}->root);
+    return __PACKAGE__->new($self->dom->[0]->root);
   }
   
   sub text_is {
     my ($self, $value, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::is $self->dom->text, $value, $desc || 'exact match for text';
+    Test::More::is $self->dom->[0]->text, $value, $desc || 'exact match for text';
     return $self;
   }
   
   sub text_isnt {
     my ($self, $value, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::isnt $self->dom->text, $value, $desc || 'no match for text';
+    Test::More::isnt $self->dom->[0]->text, $value, $desc || 'no match for text';
     return $self;
   }
   
   sub text_like {
     my ($self, $value, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::like $self->dom->text, $value, $desc || 'text is similar';
+    Test::More::like $self->dom->[0]->text, $value, $desc || 'text is similar';
     return $self;
   }
   
   sub text_unlike {
     my ($self, $value, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::unlike $self->dom->text, $value, $desc || 'text is not similar';
+    Test::More::unlike $self->dom->[0]->text, $value, $desc || 'text is not similar';
     return $self;
   }
   
   sub attr_is {
     my ($self, $name, $value, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::is $self->dom->attrs($name),
+    Test::More::is $self->dom->[0]->attrs($name),
                                 $value, $desc || 'exact match for attr value';
     return $self;
   }
@@ -96,7 +99,7 @@ use Mojo::DOM;
   sub attr_isnt {
     my ($self, $name, $value, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::isnt $self->dom->attrs($name),
+    Test::More::isnt $self->dom->[0]->attrs($name),
                                     $value, $desc || 'no match for attr value';
     return $self;
   }
@@ -104,7 +107,7 @@ use Mojo::DOM;
   sub attr_like {
     my ($self, $name, $value, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::like $self->dom->attrs($name),
+    Test::More::like $self->dom->[0]->attrs($name),
                                       $value, $desc || 'attr value is similar';
     return $self;
   }
@@ -112,7 +115,7 @@ use Mojo::DOM;
   sub attr_unlike {
     my ($self, $name, $value, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::unlike $self->dom->attrs($name),
+    Test::More::unlike $self->dom->[0]->attrs($name),
                                   $value, $desc || 'attr value is not similar';
     return $self;
   }
@@ -120,7 +123,7 @@ use Mojo::DOM;
   sub has_attr {
     my ($self, $name, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::ok defined $self->dom->attrs($name),
+    Test::More::ok defined $self->dom->[0]->attrs($name),
                                             $desc || qq/has attribute "$name"/;
     return $self;
   }
@@ -128,7 +131,7 @@ use Mojo::DOM;
   sub has_attr_not {
     my ($self, $name, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::ok ! defined $self->dom->attrs($name),
+    Test::More::ok ! defined $self->dom->[0]->attrs($name),
                                         $desc || qq/has attribute "$name" not/;
     return $self;
   }
@@ -136,7 +139,7 @@ use Mojo::DOM;
   sub has_child {
     my ($self, $selector, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::ok $self->dom->at($selector),
+    Test::More::ok $self->dom->[0]->at($selector),
                                             $desc || qq/has child "$selector"/;
     return $self;
   }
@@ -144,7 +147,7 @@ use Mojo::DOM;
   sub has_child_not {
     my ($self, $selector, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::ok !$self->dom->at($selector),
+    Test::More::ok !$self->dom->[0]->at($selector),
                                         $desc || qq/has child "$selector" not/;
     return $self;
   }
@@ -152,7 +155,7 @@ use Mojo::DOM;
   sub has_class {
     my ($self, $name, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    my $len = scalar grep {$_ eq $name} (split(/\s/, $self->dom->attrs('class')));
+    my $len = scalar grep {$_ eq $name} (split(/\s/, $self->dom->[0]->attrs('class')));
     Test::More::ok($len, $desc || qq/has child "$name"/);
     return $self;
   }
@@ -160,7 +163,7 @@ use Mojo::DOM;
   sub has_class_not {
     my ($self, $name, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    my $len = scalar grep {$_ eq $name} (split(/\s/, $self->dom->attrs('class')));
+    my $len = scalar grep {$_ eq $name} (split(/\s/, $self->dom->[0]->attrs('class')));
     Test::More::ok(! $len, $desc || qq/has child "$name"/);
     return $self;
   }
