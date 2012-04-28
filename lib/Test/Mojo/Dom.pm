f0ca2c59d97ba7fd401e284d74de0ef75b757db5
sugama <sugama@sv02.cutout.biz>
2012-04-28 17:14:57 +0900

    impremented more

--- lib/Test/Mojo/Dom.pm
+++ lib/Test/Mojo/Dom.pm
@@ -1,15 +1,7 @@
 package Test::Mojo::Dom;
 use Mojo::Base 'Test::Mojo';
   
-  sub dom_text_is {
-    my ($self, $selector, $expected, $desc) = @_;
-    local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::is $self->tx->res->dom->at($selector)->text, $expected,
-      $desc || qq/"$selector" exists/;
-    return $self;
-  }
-  
-  sub dom_sub_test {
+  sub test_dom {
     my ($self, $cb) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
     $cb->(Test::Mojo::Dom::_Test->new($self->tx->res->dom));
@@ -67,17 +59,104 @@ use Mojo::DOM;
     return $self;
   }
   
+  sub text_isnt {
+    my ($self, $value, $desc) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    Test::More::isnt $self->dom->text, $value, $desc || 'no match for text';
+    return $self;
+  }
+  
   sub text_like {
     my ($self, $value, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::like $self->dom->text, $value, $desc || 'exact match for text';
+    Test::More::like $self->dom->text, $value, $desc || 'text is similar';
+    return $self;
+  }
+  
+  sub text_unlike {
+    my ($self, $value, $desc) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    Test::More::unlike $self->dom->text, $value, $desc || 'text is not similar';
     return $self;
   }
   
   sub attr_is {
     my ($self, $name, $value, $desc) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    Test::More::is $self->dom->attrs($name), $value, $desc || 'exact match for attr value';
+    Test::More::is $self->dom->attrs($name),
+                                $value, $desc || 'exact match for attr value';
+    return $self;
+  }
+  
+  sub attr_isnt {
+    my ($self, $name, $value, $desc) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    Test::More::isnt $self->dom->attrs($name),
+                                    $value, $desc || 'no match for attr value';
+    return $self;
+  }
+  
+  sub attr_like {
+    my ($self, $name, $value, $desc) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    Test::More::like $self->dom->attrs($name),
+                                      $value, $desc || 'attr value is similar';
+    return $self;
+  }
+  
+  sub attr_unlike {
+    my ($self, $name, $value, $desc) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    Test::More::unlike $self->dom->attrs($name),
+                                  $value, $desc || 'attr value is not similar';
+    return $self;
+  }
+  
+  sub has_attr {
+    my ($self, $name, $desc) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    Test::More::ok defined $self->dom->attrs($name),
+                                            $desc || qq/has attribute "$name"/;
+    return $self;
+  }
+  
+  sub has_attr_not {
+    my ($self, $name, $desc) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    Test::More::ok ! defined $self->dom->attrs($name),
+                                        $desc || qq/has attribute "$name" not/;
+    return $self;
+  }
+  
+  sub has_child {
+    my ($self, $selector, $desc) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    Test::More::ok $self->dom->at($selector),
+                                            $desc || qq/has child "$selector"/;
+    return $self;
+  }
+  
+  sub has_child_not {
+    my ($self, $selector, $desc) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    Test::More::ok !$self->dom->at($selector),
+                                        $desc || qq/has child "$selector" not/;
+    return $self;
+  }
+  
+  sub has_class {
+    my ($self, $name, $desc) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    Test::More::ok grep {$_ eq $name} (split(/\s/, $self->dom->attrs('class'))),
+                                                $desc || qq/has child "$name"/;
+    return $self;
+  }
+  
+  sub has_class_not {
+    my ($self, $name, $desc) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    Test::More::ok ! grep {$_ eq $name} (split(/\s/, $self->dom->attrs('class'))),
+                                                $desc || qq/has child "$name"/;
     return $self;
   }
 
---
+++ t/basic.t
@@ -0,0 +1,74 @@
+package Template_Basic;
+use strict;
+use warnings;
+use utf8;
+use lib 'lib';
+use Test::More;
+use Test::Mojo::Dom;
+    
+    use Test::More tests => 33;
+
+    my $t;
+	
+    $t = Test::Mojo::Dom->new(MyApp->new);
+	$t->get_ok('/')
+		->status_is(200)
+        ->test_dom(sub {
+            my $t = shift;
+            $t->at('a')
+                ->attr_is('href', '../')
+                ->attr_isnt('href', './')
+                ->attr_like('href', qr'\.\./')
+                ->attr_unlike('href', qr'\.\./a')
+                ->text_is('some link')
+                ->text_isnt('some link2')
+                ->text_like(qr'some')
+                ->text_unlike(qr'some2')
+				->has_attr('href')
+				->has_attr('empty')
+				->has_attr_not('not_exists');
+            $t->find('a')->each(sub {
+				my $t = shift;
+				$t->text_like(qr{.});
+				$t->text_unlike(qr{a});
+				$t->attr_like('href', qr{.});
+				$t->attr_unlike('href', qr{a});
+			});
+			$t->at('a')->parent->attr_is('id', 'some_p');
+			$t->at('a')->parent->parent->attr_is('id', 'wrapper');
+			$t->at('#some_p')->has_child('a');
+			$t->at('#some_p2')->has_child_not('a');
+			
+			$t->at('#some_img')->has_class('class1');
+			$t->at('#some_img')->has_class('class2');
+			$t->at('#some_img')->has_class('class3');
+			$t->at('#some_img')->has_class_not('class4');
+        });
+
+package MyApp;
+use strict;
+use warnings;
+use base 'Mojo';
+
+	sub handler {
+		my ($self, $tx) = @_;
+		$tx->res->code(200);
+		$tx->res->body(<<'EOF');
+<body>
+	<div id="wrapper">
+		<p id="some_p">
+			<a href="../" empty="">some link</a>
+			<a href="../">some link2</a>
+			<a href="../">some link3</a>
+		</p>
+		<p id="some_p2">
+		</p>
+	</div>
+	<img id="some_img" src="#" class="class1 class2 class3" />
+</body>
+EOF
+		$tx->resume;
+	}
+
+
+__END__
